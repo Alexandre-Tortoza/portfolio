65d089044a71524283cb5365dc2ee43c6f372e8c
Alexandre Marques Tortoza Canoa <alexandre.tortoza@proton.me>
2026-01-09 18:53:33 -0300

    update: custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
       vite: { plugins: [tailwindcss()] },
-      site: 'https://alexandre-tortoza.github.io',
-      base: '/portfolio',
+      site: 'https://alexandre-tortoza.tech',
 });
