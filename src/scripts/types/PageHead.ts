024ab784007f82ac9a74971f7cdd100a965f17ea
Alexandre Marques Tortoza Canoa <alexandre.tortoza@proton.me>
2026-01-09 19:05:08 -0300

    version 2.0

--- src/scripts/types/PageHead.ts
+++ src/scripts/types/PageHead.ts
@@ -1,4 +1,9 @@
 export type PageHead = {
   lang: "en" | "pt-br";
   title: string;
+  description?: string;
+  keywords?: string;
+  image?: string;
+  url?: string;
+  type?: "website" | "article";
 };
